645bc312f8fd245b29ca7b85546c399f13f638d0
Sinelnikov Roman <sinelnikov.rv@gmail.com>
2015-05-29 12:10:25 +0300

    draft 1.1

--- todo.js
+++ todo.js
@@ -1,3 +1,20 @@
 /**
  * Sinelnikov Roman
  */
+var todo;
+todo = {
+    list: [],
+    done: [],
+    deleted: [],
+    addItem: function (item) {
+        list.push(item);
+    },
+    deleteItem: function(number){
+        list.slice(number)
+    },
+    doneItem: function(item){
+        done.push(item)
+    }
+};
+todo.addItem("test");
+console.log(todo.list);
