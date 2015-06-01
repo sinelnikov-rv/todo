cc8b5f4a49240312913f20b62f736f44b1f8c2d2
Sinelnikov Roman <sinelnikov.rv@gmail.com>
2015-06-01 10:05:16 +0300

    draft 1.2

--- todo.js
+++ todo.js
@@ -1,20 +1,35 @@
 /**
  * Sinelnikov Roman
  */
-var todo;
-todo = {
-    list: [],
-    done: [],
-    deleted: [],
-    addItem: function (item) {
-        list.push(item);
-    },
-    deleteItem: function(number){
-        list.slice(number)
-    },
-    doneItem: function(item){
-        done.push(item)
+
+        var todo;
+        todo = {
+            list: [],
+            done: [],
+            deleted: [],
+            addItem: function (item) {
+                this.list.push(item);
+            },
+            deleteItem: function (item) {
+                this.list.splice(item,1)
+                this.deleted.push(item);
+            },
+            doneItem: function (item) {
+                this.done.push(item);
+            }
+        };
+for (var i=1;i<2;) {
+    var item = prompt("Список дел: \n" + todo.list + "\n Введите задание в поле ниже:");
+    if (todo.list.indexOf(item) === -1) {
+        todo.addItem(item);
+    } else {
+        todo.deleteItem(item);
+    }
+    var answer;
+    answer = confirm("Желаете продолжить");
+    if (!answer) {
+        i++
     }
 };
-todo.addItem("test");
 console.log(todo.list);
+console.log(todo.deleted);
