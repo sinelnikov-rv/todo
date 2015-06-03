dda6482456a563e77da6c32d74119459e91e47f8
Sinelnikov Roman <sinelnikov.rv@gmail.com>
2015-06-03 21:48:15 +0300

    final draft

--- todo.js
+++ todo.js
@@ -1,44 +1,30 @@
 /**
  * Sinelnikov Roman
  */
-
-        var todo;
-        todo = {
-            list: [],
-            done: [],
-            deleted: [],
-            addItem: function (item) {
-                this.list.push(item);
+var todo = {
+        list: [],
+        done: [],
+        deleted: [],
+        addItem: function (item) {
+            this.list.push(item);
             },
-            deleteItem: function (item) {
-                this.list.splice(item,1);
+        deleteItem: function (item) {
+            this.list.splice(item,1);
             },
-            doneItem: function (item) {
-                this.done.push(item);
-            }
         };
 var text = document.getElementById("text");
 var ul = document.getElementById("list");
 button.onclick = function () {
-    if(text.value == ""){
-        console.log("sdfs");
-    }else {
-        var li = document.createElement("li")
-    todo.addItem(text.value);
-        li.setAttribute("id","element ");
-    console.log(todo.list);
-    li.appendChild(document.createTextNode("" + text.value));
-    ul.appendChild(li);
-    li.addEventListener('click',function(){
-            li.parentNode.removeChild(li);
-        });
+    if(text.value != ""){
+        var li = document.createElement("li");
+        todo.addItem(text.value);
+        li.appendChild(document.createTextNode("" + text.value));
+        ul.appendChild(li);
+        li.addEventListener('click',function(){
+            this.parentNode.removeChild(this);
+            todo.deleteItem(todo.list.indexOf(this.textContent));
+    });
     }
+    console.log(todo.list);
     text.value = "";
 };
-/*
-var checked = document.getElementById("list").document.getElementsByTagName("li");
-for (var i=0; i<checked.length;i++) {
-    checked[i].addEventListener('click', function () {
-        console.log("ok")
-    }, false);
-}*/
