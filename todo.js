544c145c511865625a4c09e789b1b6115185804e
Sinelnikov Roman <sinelnikov.rv@gmail.com>
2015-06-04 16:14:05 +0300

    add table

--- todo.js
+++ todo.js
@@ -4,22 +4,32 @@
 var todo = {
         list: [],
         done: [],
-        deleted: [],
         addItem: function (item) {
             this.list.push(item);
             },
         deleteItem: function (item) {
             this.list.splice(item,1);
-            }
+            },
+        doneItem: function (item) {
+            this.done.push(item);
+        }
         };
 var text = document.getElementById("text");
 var ul = document.getElementById("list");
+var ulDone = document.getElementById("doneList");
 var clickedElement = function () {
     if(this.getAttribute("class") == "checked") {
         this.parentNode.removeChild(this);
         todo.deleteItem(todo.list.indexOf(this.textContent))
     } else {
+        var li = document.createElement("li");
         this.setAttribute("class","checked");
+        li.setAttribute("class","checked");
+        li.appendChild(document.createTextNode("" + this.textContent))
+        //console.log(this.value);
+        ulDone.appendChild(li);
+        li.addEventListener('click', clickedElement);
+        todo.doneItem(this.textContent);
     }
 };
 button.onclick = function () {
@@ -32,5 +42,6 @@ button.onclick = function () {
         li.addEventListener('click', clickedElement);
     }
     console.log(todo.list);
+    console.log(todo.done);
     text.value = "";
 };
