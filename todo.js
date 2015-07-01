26005e8b23a212f7abd6e243942a07acb5f194ed
Sinelnikov Roman <sinelnikov.rv@gmail.com>
2015-07-01 17:18:02 +0300

    Drag N Drop doesn`t work

--- todo.js
+++ todo.js
@@ -4,139 +4,144 @@
 var Task = function (text, listName) {
     this.id = new Date().valueOf();
     this.text = text;
-    this.check = false;
+    this.checked = false;
     this.list = listName;
     this.liElem = document.createElement("li");
     this.liElem.innerHTML ="<div class='unChecked'><input type = 'checkbox'>" + this.text + "</div><button class ='delButton'>del</button>";
-    //this.liElem.getElementsByClassName("unChecked")[0].addEventListener('click', this.check.bind(this));
-    this.liElem.getElementsByClassName("delButton")[0].addEventListener('click', this.del.bind(this));
 };
-/*
 Task.prototype.check = function () {
-    if (this.check) {
-        this.check = false;
+    if (this.checked) {
+        this.checked = false;
+        this.liElem.getElementsByTagName('input')[0].checked = false;
+        this.liElem.getElementsByTagName("div")[0].setAttribute("class","unChecked");
+        this.liElem.removeChild(this.liElem.getElementsByClassName("move")[0]);
     } else {
-        this.check = true;
-    }
-
-
-    if(this.liElem.getElementsByTagName("div")[0].getAttribute("class") === "unChecked") {
         this.liElem.getElementsByTagName("input")[0].checked = true;
         this.liElem.getElementsByTagName("div")[0].setAttribute("class","checked");
         var move = document.createElement("button");
         move.setAttribute("class","move");
         this.liElem.insertBefore(move,this.liElem.getElementsByClassName("delButton")[0]);
-        this.liElem.getElementsByClassName("move")[0].addEventListener('click', this.move.bind(this));
-        if(this.liElem.parentNode.getAttribute("id") === "list"){
-            this.liElem.getElementsByClassName("move")[0].innerHTML = "-->";
-        }
-        else {
-            this.liElem.getElementsByClassName("move")[0].innerHTML = "<--";
-        }
+        this.liElem.getElementsByClassName("move")[0].innerHTML = "Move";
+        listenersMove(this);
+        this.checked = true;
     }
-    else {
-        this.liElem.getElementsByTagName("input")[0].checked = false;
-        this.liElem.getElementsByTagName("div")[0].setAttribute("class","unChecked");
-        this.liElem.removeChild(this.liElem.getElementsByClassName("move")[0]);
-    }
+
 };
-Task.prototype.move = function (oldList, newList) {
+Task.prototype.move = function (oldList,newList) {
+        newList.list.push(this);
+        oldList.deleteItem(this);
+};
 
-    oldlist.deleteItem(this.liElem);
-    newlist.push(this.liElem);
-
-    if(this.liElem.parentNode.getAttribute("id") === "list"){
-        //var doneTask = new Task(this.liElem.childNodes[0].textContent);
-        document.getElementById('list_1').appendChild(doneTask.liElem);
-        done.list.push(this.liElem.childNodes[0].textContent);
-        todo.deleteItem(this.liElem);
-    }
-    else {
-        var task = new Task(this.liElem.childNodes[0].textContent);
-        document.getElementById('list').appendChild(task.liElem);
-        todo.list.push(this.liElem.childNodes[0].textContent);
-        done.deleteItem(this.liElem);
-    }
- };
-*/
-
-
-
-Task.prototype.del = function () {
-    var listName = this.list;
-    if(listName === 'todo') {
-        todo.deleteItem(this);
-    }
-    else {
-        done.deleteItem(this);
-    }
+Task.prototype.del = function (listName){
+        listName.deleteItem(this);
 };
 var todo = {
     list : [],
     addItem: function(textOfTask) {
-        var task = new Task(textOfTask.value, 'todo');
-        console.log(todo.list[0]);
+        var task = new Task(textOfTask, 'todo');
+        listeners(task);
+
         todo.list.push(task);
-        console.log(task);
         document.getElementById('list').appendChild(task.liElem);
-        console.log(todo.list.indexOf(task.text));
-        console.dir(todo.list);
     },
     deleteItem: function (item) {
-        //console.log(item.liElem);
         item.liElem.parentNode.removeChild(item.liElem);
-        var item = todo.list.indexOf(item.id);
-        todo.list.splice(item, 1);
-        console.dir(todo.list);
+        todo.list.splice(todo.list.indexOf(item.id), 1);
+    }
+};
+var done = {
+    list: [],
+    addItem: function (textOfTask) {
+                var doneTask = new Task(textOfTask, 'done');
+        listeners(doneTask);
+                document.getElementById('list_1').appendChild(doneTask.liElem);
+                done.list.push(doneTask);
+    },
+    deleteItem: function (item) {
+        item.liElem.parentNode.removeChild(item.liElem);
+        done.list.splice(done.list.indexOf(item.id), 1);
     }
 };
 
-/*var done = {
-    list: [],
-    addItem: function () {
-        var text = document.getElementById("textOfTask");
-        if (text.value !== "") {
-            if (todo.list.indexOf(text.value) > -1 || done.list.indexOf(text.value) > -1) {
-                document.getElementById("alert").innerHTML = "<p>такой элемент уже существует</p>";
-            }
-            else {
-                document.getElementById("alert").innerHTML = "";
-                var doneTask = new Task(text.value);
-                document.getElementById('list_1').appendChild(doneTask.liElem);
-                done.list.push(text.value);
-            }
+document.getElementById('textOfTask').onkeydown = addElem;
+document.getElementById('add').onclick = addElem;
+
+function addElem(event) {
+    var text = document.getElementById("textOfTask");
+    if (text.value && +event.keyCode === 13 ||text.value && event.type === 'click') {
+        textExist(text.value);
+        if (!textExists && event.keyCode === 13) {
+            todo.addItem(text.value);
+            text.value = "";
+        } else if(!textExists && event.type === 'click') {
+            done.addItem(text.value);
             text.value = "";
         }
-    },
-    deleteItem: function (item) {
-        item.parentNode.removeChild(item);
-        item = done.list.indexOf(item.childNodes[0].textContent);
-        done.list.splice(item, 1);
-    }
-};*/
-document.getElementById('textOfTask').onkeydown = addElem;
-function addElem(key) {
-    var text = document.getElementById("textOfTask");
-    if (text.value !== "" && +key.keyCode === 13) {
-        if (todo.list.indexOf(Task.text) > -1 /*|| done.list.indexOf(text.value) > -1*/) {
-            document.getElementById("alert").innerHTML = "<p>такой элемент уже существует</p>";
-        }
-        else {
-            document.getElementById("alert").innerHTML = "";
-            todo.addItem(text);
-
-        }
-        //text.value = "";
     }
 }
-//document.getElementById('add').onclick = done.addItem;
+var textExists = false;
+var lists = [todo.list, done.list];
+function textExist(text)
+{
+    lists.forEach(function (list) {
+        list.forEach(function (task) {
+            if (text == task.text) {
+                textExists = true;
+                document.getElementById("alert").innerHTML = "<p>такой элемент уже существует</p>";
+            }
+            else{
+                textExists = false;
+                document.getElementById("alert").innerHTML = "";
+            }
+        })
+    });
+}
+function listeners(task) {
+    task.liElem.getElementsByClassName("unChecked")[0].addEventListener('click', function () {
+        task.check();
+    });
+    task.liElem.getElementsByClassName("delButton")[0].addEventListener('click', function () {
+        if(task.list === 'todo') {
+            task.del(todo);
+        } else {
+            task.del(done);
+        }
+    });
+    task.liElem.getElementsByClassName("unChecked")[0].addEventListener('onmousedown', function () {
+        task.drag(task);
+    });
+}
+function listenersMove(task){
+    task.liElem.getElementsByClassName("move")[0].addEventListener('click', function(){
+        if(task.list === 'todo'){
+            task.list = 'done';
+            task.check();
+            task.move(todo,done);
+            document.getElementById('list_1').appendChild(task.liElem);
+        } else {
+                task.list = 'todo';
+            task.check();
+            task.move(done,todo);
+            document.getElementById('list').appendChild(task.liElem);
+            }
+    });
+}
 
-
-/*
-function listenters(task, todo, done) {
-    task.liElem.addEventListener('click', function () {
-        bind(task)
-        task.move(todo.list, )
-    })
-// add all event listeners
-}*/
+Task.prototype.drag = function (e) {
+    console.log(this);
+    this.liElem.style.position = 'absolute';
+    moveAt(e);
+    document.body.appendChild(this.liElem);
+    this.liElem.style.zIndex = 1000;
+    function moveAt(e) {
+        this.liElem.style.left = e.pageX - this.liElem.offsetWidth / 2 + 'px';
+        this.liElem.style.top = e.pageY - this.liElem.offsetHeight / 2 + 'px';
+    }
+    document.onmousemove = function(e) {
+        moveAt(e);
+    };
+    this.liElem.onmouseup = function() {
+        document.onmousemove = null;
+        this.liElem.onmouseup = null;
+    }
+};
